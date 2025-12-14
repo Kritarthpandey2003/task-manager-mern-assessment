443f062309a2a2ea94903bca458ff92a9ccd7bf6
Kritarth Pandey <kritarthpandey2003@gmail.com>
2025-12-14 23:25:21 +0530

    feat: configure server for deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,7 +6,7 @@ import { Pool } from 'pg';
 dotenv.config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // Connect to Neon Database
 const pool = new Pool({
